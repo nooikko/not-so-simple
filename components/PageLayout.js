1021f6ed2272a419124ea54939a37621d9852d1f
Elijah Penney <elijah.penney@datahshieldprotect.com>
2021-01-10 19:19:05 -0700

    Add github and credit

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -3,7 +3,7 @@ import { Logo } from '../components';
 
 export const PageLayout = ({ children }) => {
   return (
-    <div className='w-100 h-full flex flex-col bg-gray-100 pb-12'>
+    <div className='w-100 h-full flex flex-col bg-gray-100 pb-4'>
       <div className='container mx-auto'>
         <div className='flex flex-row justify-between py-3'>
           <Logo />
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,27 @@ const Home = () => {
           Simple is closing.
         </a>{' '}
         Here's some alternatives.
+        <div className='w-100 flex justify-center py-6 text-lg'>
+          <span>
+            <a
+              href='https://www.reddit.com/r/SimpleBanking/comments/ksjpw5/megathread_for_discussing_alternatives/gigdx7f?utm_source=share&utm_medium=web2x&context=3'
+              target='_blank'
+              rel='noreferrer'
+              className='text-purple-600 underline'
+            >
+              Original Data
+            </a>{' '}
+            by{' '}
+            <a
+              className='text-purple-600 underline'
+              href='https://www.reddit.com/user/11jwolfe2/'
+            >
+              11jwolfe2
+            </a>
+          </span>
+        </div>
       </div>
+
       <div className='grid grid-cols-3 gap-4 pt-6'>
         {restBanks.map((data) => (
           <Card key={data.name} {...data} simple={simpleBank[0]} />
